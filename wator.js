879cb44ef99e92f8544ec18e9abbf3fa1f154e55
svdt <sebastian.vonderthannen@gmail.com>
2017-03-06 10:50:47 +0100

    Update wator.js

--- wator.js
+++ wator.js
@@ -7,7 +7,7 @@ var App = {
 	CANVAS_WIDTH : 1100,
 	CANVAS_HEIGHT : 600,
 	CANVAS_ID : "canvas",
-  CELL : 50,
+  	CELL : 50,
 	XSTEP : 22,
 	YSTEP : 12,
 	NSHARK : 5,
@@ -16,22 +16,22 @@ var App = {
 
 App.Start = function(){
 	canvas = document.getElementById(this.CANVAS_ID);
-    ctx = canvas.getContext('2d');
+    	ctx = canvas.getContext('2d');
 
-    App.Init();
+  	App.Init();
 
-    Pos = App.getRandPos();
+	Pos = App.getRandPos();
 
-    sharks = [];
-    for(var i = 0; i < this.NSHARK; i++){
-      sharks.push(new Shark(Math.random(),Pos[i]));
-    }
-    fishes = [];
-    for(var i = this.NSHARK; i < this.NSHARK + this.NFISH; i++){
-      fishes.push(new Fish(Math.random(),Pos[i]));
-    }
+	sharks = [];
+	for(var i = 0; i < this.NSHARK; i++){
+	sharks.push(new Shark(Math.random(),Pos[i]));
+	}
+	fishes = [];
+	for(var i = this.NSHARK; i < this.NSHARK + this.NFISH; i++){
+	fishes.push(new Fish(Math.random(),Pos[i]));
+	}
 
-    lastUpdate = Date.now();
+	lastUpdate = Date.now();
 
 	setTimeout("App.Run()", this.INTERVAL);
 };
@@ -67,44 +67,42 @@ App.Run = function(){
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 	//ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  for(var i = 0; i < this.NFISH; i++){
-    fishes[i].Draw(ctx);
-  }
-  for(var i = 0; i < this.NSHARK; i++){
-    sharks[i].Draw(ctx);
-  }
-  var now = Date.now();
+	for(var i = 0; i < this.NFISH; i++){
+		fishes[i].Draw(ctx);
+	}
+	for(var i = 0; i < this.NSHARK; i++){
+		sharks[i].Draw(ctx);
+	}
+	var now = Date.now();
 	if (now - lastUpdate > 300) {
 		App.fishSwim(fishes,sharks);
 		lastUpdate = now;
 	}
 
-  setTimeout("App.Run()", this.INTERVAL);
+ 	setTimeout("App.Run()", this.INTERVAL);
 };
 
 
 App.fishSwim = function(f,s){
-  for(var i = 0; i < f.length; i++){
-    var moveX = (Math.round(Math.random())*2-1)*this.CELL;
-    var moveY = (Math.round(Math.random())*2-1)*this.CELL;
-    f[i].UpdatePosition(f[i].position.x+moveX,f[i].position.y+moveY)
-  }
+	for(var i = 0; i < f.length; i++){
+		var moveX = (Math.round(Math.random())*2-1)*this.CELL;
+		var moveY = (Math.round(Math.random())*2-1)*this.CELL;
+		f[i].UpdatePosition(f[i].position.x+moveX,f[i].position.y+moveY)
+	}
 }
 // utility stuff
 App.getRandPos = function(){
-  r = [];
-  while(r.length < this.NSHARK+this.NFISH){
-    var trig = false;
-    var x = Math.ceil(Math.random()*(this.XSTEP-1));
-    var y = Math.ceil(Math.random()*(this.YSTEP-1));
-    pos = new Vec2(this.CELL*x, this.CELL*y); // cells have 40px width/height
-    for(var i = 0; i < r.length; i++){
-      if(r[i].x == pos.x && r[i].y == pos.y){
-        trig = true;
-      }
-    }
-    if(trig) continue;
-    r[r.length] = pos;
-  }
-  return r;
+	r = [];
+	while(r.length < this.NSHARK+this.NFISH){
+		var trig = false;
+		var x = Math.ceil(Math.random()*(this.XSTEP-1));
+		var y = Math.ceil(Math.random()*(this.YSTEP-1));
+		pos = new Vec2(this.CELL*x, this.CELL*y); // cells have 40px width/height
+		for(var i = 0; i < r.length; i++){
+			if(r[i].x == pos.x && r[i].y == pos.y) trig = true;
+		}
+		if(trig) continue;
+		r[r.length] = pos;
+	}
+	return r;
 }
